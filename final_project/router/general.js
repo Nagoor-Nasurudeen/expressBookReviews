090776eb2a4b7a60b3b21252b2b7b7523e91c36a
NAGOOR NASURUDEEN O M <nagoornasurudeen@gmail.com>
2026-01-18 13:01:30 +0530

    implemented get books,by isbn,by author
    Implemented endpoints to retrieve book list, details by ISBN, and books by author.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,19 +22,27 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn=req.params.isbn
+  if(!books[isbn]) return res.status(404).json({message:`There is no book for the given ISBN code ${isbn}`})
+
+  return res.send(books[isbn]);
  });
   
-// Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  let author=req.params.author.toLowerCase()
+  const result=[]
+  for(let isbn in books){
+    if (books[isbn].author.toLowerCase()===author) result.push(books[isbn])
+  }
+  if(result) return res.send(result)
+  return res.status(404).json({message: `There is no book for the given author ${author}`});
 });
 
 // Get all books based on title

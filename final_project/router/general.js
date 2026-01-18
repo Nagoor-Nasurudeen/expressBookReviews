c11ffa616289b6abbb1fa5779894bb69b4e91310
Nagoor=Nasurudeen <nagoornasurudeen@gmail.com>
2026-01-18 05:15:26 -0500

    finished general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,7 +29,7 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   let isbn=req.params.isbn
-  if(!books[isbn]) return res.status(404).json({message:`There is no book for the given ISBN code ${isbn}`})
+  if(!books[isbn]) return res.status(404).json({message:`There is no book for the given ISBN code = ${isbn}`})
 
   return res.send(books[isbn]);
  });
@@ -39,22 +39,34 @@ public_users.get('/author/:author',function (req, res) {
   let author=req.params.author.toLowerCase()
   const result=[]
   for(let isbn in books){
-    if (books[isbn].author.toLowerCase()===author) result.push(books[isbn])
+    if (books[isbn].author.toLowerCase()===author) result.push({isbn,...books[isbn]})
   }
-  if(result) return res.send(result)
+  if(result.length>0) return res.send(result)
   return res.status(404).json({message: `There is no book for the given author ${author}`});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title=req.params.title.toLowerCase()
+  const result=[]
+  for(let isbn in books){
+    if (books[isbn].title.toLowerCase()===title) result.push({isbn,...books[isbn]})
+  }
+  if(result.length>0) return res.send(result)
+  return res.status(404).json({message: `There is no book for the given title ${title}`});
+  
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn=req.params.isbn
+    if(books[isbn]) {
+        if(Object.entries(books[isbn].reviews).length===0) return res.send("There are no reviews  available now")
+        return res.send(books[isbn].reviews)
+}
+  return res.status(404).json({message:`There is no book for the given ISBN code = ${isbn}`})
 });
 
 module.exports.general = public_users;
